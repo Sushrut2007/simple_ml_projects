91093815227b423a7b344c223fc1b7812b88a488
Sushrut <sushrutdeshmukh4@gmail.com>
2026-08-31 16:06:45 +0530

    Feat: Add function to validate dataset with correct format

--- PolyFit/src/utils.py
+++ PolyFit/src/utils.py
@@ -3,6 +3,7 @@ Utility functions
 
 """
 
+import numpy as np
 import pandas as pd
 
 
@@ -19,4 +20,29 @@ def load_dataset(filename):
     return df
 
 
+# -----------------------------
+# Dataset validation
+#------------------------------
 
+def validate_dataset(df):
+    """
+    Validate the dataset against the requirements of Polynomial Regression.
+
+    The function passes through multiple checks:
+        1. Check if the df contains other than numeric values.
+        2. Check if the df contains atleast 2 columns.
+    
+    Return True for valid dataset.
+    """
+
+    # Check if the df contains other than numeric values
+    allowed_dtypes = ['int64', 'int32', 'float64', 'float32', 'category']
+    df_dtypes = df.dtypes
+
+    # Check if the df contains atleast 2 columns
+    num_cols = df.shape[1]
+
+    if not all(str(dtype) in allowed_dtypes for dtype in df_dtypes):
+        return "Please use a valid dataset!"
+    
+    return True
