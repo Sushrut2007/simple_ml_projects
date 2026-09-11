eea06f3095abcd00ce81908c180d8b0dc07887c4
Sushrut <sushrutdeshmukh4@gmail.com>
2026-09-11 13:26:19 +0530

    Wrapped up form filling in a function

--- PolyFit/app.py
+++ PolyFit/app.py
@@ -1,18 +1,59 @@
 import streamlit as st
 from src.utils import *
 
+
+# Initialize session states
+if 'settings' not in st.session_state:
+    st.session_state.settings = None
+
+
 st.title('PolyFit')
 st.caption('Know the best degree complexity for your Polynomial Regression!')
 st.divider()
 
-# --------------------
-# User inputs
-# --------------------
-st.header('Step 1')
+def set_model_settings():
+    """
+    Generate a form for the model settings upon selection of valid dataset.
+    """
 
-# File uploader
+    with st.form('polyfit_model_settings'):
+        # Selection of target variable
+        st.header('Step 1')
+
+        target = st.selectbox('Target variable', options=[column for column in df.columns],
+                            help='The target value to predict by the model')
+
+
+        st.divider()
+        # Maximum degree model, n = max_degree i.e n models will be trained (degree 1 to n)
+        st.header('Step 2')
+        
+        max_degree = st.slider('Select the maximum degree of polynomial',
+                            min_value=1, max_value=20, step=1,
+                            help='n models will be trained, degree 1 to n.')
+
+                            
+        st.divider()
+        # Tolerence in %, which can allow even a high error model that is less complex.
+        st.header('Step 3')
+
+        tolerance = st.slider('Select a tolerance in %', 
+                            min_value=0, max_value=100, help='Maximum percentage increase in RMSE allowed compared with the best-performing model. A higher tolerance favors simpler polynomial degrees.')
+
+        # Submit button
+        submitted = st.form_submit_button('Finalize dataset and model settings')
+
+        # Return settings as a tuple upon submission
+        if submitted:
+            st.success('Model settings set successfully!')
+            return target, max_degree, tolerance
+
+        return None
+
+
+# Dataset uploader
 uploaded_file = st.file_uploader(
-    'Upload a clean CSV file',
+    'Upload a clean file',
     type = 'csv'
 )
 
@@ -23,40 +64,23 @@ if uploaded_file:
     # Validate dataset
     is_valid = validate_dataset(df)
 
+    # Display model settings form
     if is_valid is True:
         st.success('Valid dataset!')
+        st.divider()
+
+        settings = set_model_settings()
+
+        if settings is not None:
+            target, max_degree, tolerance = settings
+
     else:
         st.warning('Invalid dataset!')
+        st.stop()
 
 else:
     st.info('Please upload a CSV file before you can move further.')
     st.stop()
 
-
-st.divider()
-# Selection of target variable
-st.header('Step 2')
-
-target = st.selectbox('Target variable', options=[column for column in df.columns],
-                      help='The target value to predict by the model')
-
-
-st.divider()
-
-# Maximum degree model, n = max_degree i.e n models will be trained (degree 1 to n)
-st.header('Step 3')
- 
-max_degree = st.slider('Select the maximum degree of polynomial',
-                       min_value=1, max_value=20, step=1,
-                       help='n models will be trained, degree 1 to n.')
-
-                       
-st.divider()
-
-# Tolerence in %, which can allow even a high error model that is less complex.
-st.header('Step 4')
-
-tolerance = st.slider('Select a tolerance in %', 
-                      min_value=0, max_value=100, help='Maximum percentage increase in RMSE allowed compared with the best-performing model. A higher tolerance favors simpler polynomial degrees.')
 
 
